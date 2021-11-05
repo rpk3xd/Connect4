bb54e3125e34d1a1215066b841e953c1d63c9b09
Roman Kaker <rpk3xd@virginia.edu>
2021-11-05 15:45:26 -0400

    Working algorithm

--- src/Connect4.java
+++ src/Connect4.java
@@ -3,20 +3,24 @@ import java.util.*;
 public class Connect4{
 
     final int COMPUTERCHIP = 2;
+    final int PLAYERCHIP = 1;
+    final int COLNUM = 7;
+    final int ROWNUM = 6;
     int [][] grid;
     int [] col;
     int turns;
     boolean turn;
 
     public Connect4(){
-        grid = new int[7][7];
-        col = new int[7];
+        grid = new int[ROWNUM][COLNUM];
+        col = new int[COLNUM];
         turns = 0;
         //Initialize
-        for(int i = 0; i < 7; i++){
-            col[i] = 6;
-            for(int j = 0; j < 7; j++){
+        for(int i = 0; i < ROWNUM; i++){
+            
+            for(int j = 0; j < COLNUM; j++){
                 grid[i][j] = 0;
+                col[j] = 5;
             }
         }
     }
@@ -24,55 +28,47 @@ public class Connect4{
     public int [][] getGrid(){
         return grid;
     }
-
+    public boolean insert(int[][] board, int column, int [] colms, int piece){
+        boolean won = false;
+        board[colms[column]][column] = piece;
+        won = gameWon(board, piece);
+        colms[column]--;
+        return won;
+    }
     public int [] getCol(){
         return col;
     }
-
-    public boolean insert(int[][] board, int column, int [] cols){
-        boolean won = false;
-        if(turn){
-            board[cols[column]][column] = 1;
-            won = gameWon(1);
-        }else{
-            board[cols[column]][column] = 2;
-            won = gameWon(2);
-        }
-        cols[column]--;
-        return won;
-    }
-
     //false if game has not been won
-    public boolean gameWon(int playerOrComputer){
+    public boolean gameWon(int [][] board, int playerOrComputer){
         //implement function to check if game has been won
         //Horizontal
-        for(int x = 0; x < 4; x ++){
-            for(int y = 0; y < 7; y++){
-                if(grid[y][x] == playerOrComputer && grid[y][x+1] == playerOrComputer && grid[y][x+2] == playerOrComputer && grid[y][x+3] == playerOrComputer){
+        for(int x = 0; x < COLNUM-3; x ++){
+            for(int y = 0; y < ROWNUM; y++){
+                if(board[y][x] == playerOrComputer && board[y][x+1] == playerOrComputer && board[y][x+2] == playerOrComputer && board[y][x+3] == playerOrComputer){
                     return true;
                 }
             }
         }
         //Vertical
-        for(int y = 0; y < 4; y++){
-            for(int x = 0; x < 7; x++){
-                if(grid[y][x] == playerOrComputer && grid[y+1][x] == playerOrComputer && grid[y+2][x] == playerOrComputer && grid[y+3][x] == playerOrComputer){
+        for(int y = 0; y < ROWNUM-3; y++){
+            for(int x = 0; x < COLNUM; x++){
+                if(board[y][x] == playerOrComputer && board[y+1][x] == playerOrComputer && board[y+2][x] == playerOrComputer && board[y+3][x] == playerOrComputer){
                     return true;
                 }
             }
         }
         //Diagnol Increasing/Decreasing
-        for(int x = 3; x < 7; x++){
-            for(int y = 3; y < 7; y++){
-                if(grid[y][x] == playerOrComputer && grid[y-1][x-1] == playerOrComputer && grid[y-2][x-2] == playerOrComputer && grid[y-3][x-3] == playerOrComputer){
+        for(int x = 0; x < COLNUM-3; x++){
+            for(int y = 0; y < ROWNUM-3; y++){
+                if(board[y][x] == playerOrComputer && board[y+1][x+1] == playerOrComputer && board[y+2][x+2] == playerOrComputer && board[y+3][x+3] == playerOrComputer){
                     return true;
                 }
             }
         }
        
-        for(int y = 3; y < 7; y++){
-            for(int x = 0; x < 4; x++){
-                if(grid[y][x] == playerOrComputer && grid[y-1][x+1] == playerOrComputer && grid[y-2][x+2] == playerOrComputer && grid[y-3][x+3] == playerOrComputer){
+        for(int y = 3; y < ROWNUM; y++){
+            for(int x = 0; x < COLNUM-3; x++){
+                if(board[y][x] == playerOrComputer && board[y-1][x+1] == playerOrComputer && board[y-2][x+2] == playerOrComputer && board[y-3][x+3] == playerOrComputer){
                     return true;
                 }
             }
@@ -80,103 +76,152 @@ public class Connect4{
         return false;
     }
 
-    public int howMany(int [] row){
+    public int howMany(int [] row, int chip){
         int count = 0;
         for(int i : row){
-            if(i == 2){
+            if(i == chip){
                 count++;
             }
         }
         return count;
     }
-
-    public int scoreBoard(int [][] board){
+    public int scoring(int [] row, int zeroOrOne){
         int score = 0;
+        int player = howMany(row, PLAYERCHIP);
+        int computer = howMany(row, COMPUTERCHIP);
+        int empty = howMany(row, 0);
+        if(computer == 4){
+            score += 50;
+        }else if(computer == 3 && empty == 1){
+            score += 5;
+        }else if(computer == 2 && empty == 2){
+            score += 2;
+        }
+        if(player == 3 && empty == 1){
+            score -= 20;
+        }
+
+        return score;
+    }
+    public int scoreBoard(int [][] board, int chip){
+        int score = 0;
+
+        //Center column
+        int [] center = {board[0][3], board[1][3], board[2][3], board[3][3], board[4][3], board[5][3]};
+        int c = howMany(center, chip);
+        score += (c * 5);
         //Horizontal
+        //display(board);
+        //System.out.println();
         
-        int maxCount = 0;
-        
-        for(int i = 0; i < 7; i++){
-            int twosCount = 0;
-            for(int j = 0; j < 4; j++){
+        for(int i = 0; i < ROWNUM; i++){
+            for(int j = 0; j < COLNUM-3; j++){
                 int [] a = {board[i][j], board[i][j+1], board[i][j+2], board[i][j+3]};
-                twosCount = howMany(a);
-                if(twosCount > maxCount) maxCount = twosCount;
+                score += scoring(a, chip);
             }
-            if(maxCount == 4) score+=100;
-            if(maxCount == 3) score+=75;
-            if(maxCount == 2) score+=50;
         }
         
         //Vertical
-        for(int i = 0; i < 4; i++){
-            int twosCount = 0;
-            for(int j = 0; j < 7; j++){
+        for(int i = 0; i < ROWNUM-3; i++){
+            for(int j = 0; j < COLNUM; j++){
                 int [] a = {board[i][j], board[i+1][j], board[i+2][j], board[i+3][j]};
-                twosCount = howMany(a);
-                if(twosCount > maxCount) maxCount = twosCount;
+                score += scoring(a, chip);
             }
-            if(maxCount == 4) score+=100;
-            if(maxCount == 3) score+=75;
-            if(maxCount == 2) score+=50;
         }
         
-        //Diagnol Increasing Decreasing
-        for(int i = 3; i < 7; i++){
-            int twosCount = 0;
-            for(int j = 3; j < 7; j++){
-                int [] a = {board[i][j], board[i-1][j-1], board[i-2][j-2], board[i-3][j-3]};
-                twosCount = howMany(a);
-                if(twosCount > maxCount) maxCount = twosCount;
+        //Diagnal Increasing Decreasing
+        for(int i = 0; i < ROWNUM-3; i++){
+            for(int j = 0; j < COLNUM-3; j++){
+                int [] a = {board[i][j], board[i+1][j+1], board[i+2][j+2], board[i+3][j+3]};
+                score += scoring(a, chip);
             }
-            if(maxCount == 4) score+=100;
-            if(maxCount == 3) score+=75;
-            if(maxCount == 2) score+=50;
         }
-        for(int i = 3; i < 7; i++){
-            int twosCount = 0;
-            for(int j = 0; j < 4; j++){
+        for(int i = 3; i < ROWNUM; i++){
+            for(int j = 0; j < COLNUM-3; j++){
                 int [] a = {board[i][j], board[i-1][j+1], board[i-2][j+2], board[i-3][j+3]};
-                twosCount = howMany(a);
-                if(twosCount > maxCount) maxCount = twosCount;
+                score += scoring(a, chip);
             }
-            if(maxCount == 4) score+=100;
-            if(maxCount == 3) score+=75;
-            if(maxCount == 2) score+=50;
         }
-        
         //
         return score;
     }
-
-    public int generateMove(int [][] board, int [] colmn){
-        int column = 0;
-        int maxScore = 0;
-        for(int colm : colmn){
-            if(colm != -1){
-                int [][] tempGrid = generateCopyOfGrid(board);
-                int [] tempColm = Arrays.copyOf(col, 7);
-                insert(tempGrid, colm, tempColm);
-                int value = scoreBoard(tempGrid);
-                if(value > maxScore){
-                    maxScore = value;
-                    column = colm;
+    public boolean endNode(int [][] board, ArrayList<Integer> val){
+        //no more valid moves
+        return gameWon(board, PLAYERCHIP) || gameWon(board, COMPUTERCHIP) || (val.size() == 0);
+    }
+    //maximizingPlayer true when comp false when player
+    public int[] minimax(int [][] board, int[] colmn, int depth, boolean maximizingPlayer){
+        ArrayList<Integer> val = validLocations(board);
+        boolean end = endNode(board, val);
+        if(depth == 0 || end){
+            if(end){
+                if(gameWon(board, COMPUTERCHIP)){
+                    return new int [] {-1, Integer.MAX_VALUE};
+                }else if(gameWon(board, PLAYERCHIP)){
+                    return new int [] {-1, Integer.MIN_VALUE};
+                }else{
+                    return new int [] {-1, 0};
                 }
+            }else{
+                return new int [] {-1, scoreBoard(board, COMPUTERCHIP)};
             }
         }
-        return column;
+        if(maximizingPlayer){
+            int value = Integer.MIN_VALUE;
+            int index = (int)Math.random() * val.size();
+            int column = val.get(index);
+            for(int i : val){
+                int [][] tempGrid = generateCopyOfGrid(board);
+                int [] tempCol = Arrays.copyOf(colmn, 7);
+                insert(tempGrid, i, tempCol, COMPUTERCHIP);
+                int score = minimax(tempGrid, tempCol, depth-1,false)[1];
+                if(score > value){
+                    value = score;
+                    column = i;
+                }
+            }   
+            return new int[] {column, value};
+        }else{
+            int value = Integer.MAX_VALUE;
+            int index = (int)Math.random() * val.size();
+            int column = val.get(index);
+            for(int i : val){ 
+                int [][] tempGrid = generateCopyOfGrid(board);
+                int [] tempCol = Arrays.copyOf(colmn, 7);
+                insert(tempGrid, i, tempCol, PLAYERCHIP);
+                int score = minimax(tempGrid, tempCol, depth-1, true)[1];
+                if(score < value){
+                    value = score;
+                    column = i;
+                }        
+            }
+            return new int[] {column, value};
+        }
     }
-
+    public boolean valid(int [][] board, int column){
+        if(board[0][column] != 0){
+            return false;
+        }
+        return true;
+    }
+    public ArrayList<Integer> validLocations(int [][] board){
+        ArrayList<Integer> val = new ArrayList<>();
+        for(int i = 0; i < COLNUM; i ++){
+            if(valid(board, i)){
+                val.add(i);
+            }
+        }
+        return val;
+    }
     public int[][] generateCopyOfGrid(int [][] board){
-        int[][] res = new int[7][7];
+        int[][] res = new int[ROWNUM][COLNUM];
         for(int i = 0; i < board.length; i++){
-            res[i] = Arrays.copyOf(board[i], 7);
+            res[i] = Arrays.copyOf(board[i], COLNUM);
         }
         return res;
     }
-
-    public void display(){
-        for(int[] row : grid){
+    public void display(int [][] board){
+        for(int[] row : board){
             System.out.println(Arrays.toString(row));
         }
     }
--- src/Connect4Game.java
+++ src/Connect4Game.java
@@ -7,6 +7,7 @@ public class Connect4Game{
         Connect4 game = new Connect4();
         System.out.println("------- Welcome to Connect4 -------");
         Scanner sc = new Scanner(System.in);
+        int d = 1;
         while(true){
             System.out.println("Please choose who goes first: Player(p) or Computer(c)");
             String first = sc.nextLine();
@@ -21,8 +22,21 @@ public class Connect4Game{
             }
         }
         while(true){
+            System.out.println("Please choose your difficulty: Hard (h) or Easy (e)");
+            String diff = sc.nextLine();
+            if(diff.equals("h")){
+                d = 7;
+                break;
+            }else if(diff.equals("e")){
+                d = 1;
+                break;
+            }else{
+                System.out.println("Invalid Input, Try Again");
+            }
+        }
+        while(true){
             boolean isWon = false;
-            game.display();
+            game.display(game.getGrid());
             System.out.println("Here is the current game board");
             if(game.turn){
                 System.out.println("Enter which column would you like to place a piece (0-6 inclusive): ");
@@ -32,34 +46,32 @@ public class Connect4Game{
                 }else if(game.col[columnP] == -1){
                     System.out.println("Column is Full, Try Again");
                 }else{
-                    isWon = game.insert(game.getGrid(), columnP, game.getCol());
+                    isWon = game.insert(game.getGrid(), columnP, game.getCol(), 1);
                     game.turns++;
                 }
             }else{
                 
                 System.out.println("Computer is Calculating Best Move");
                 //TimeUnit.SECONDS.sleep();
-                int columnC = 0;
-                if(game.turns == 1 || game.turns == 0){
-                    columnC = 3;
-                }else{
-                    columnC = game.generateMove(game.getGrid(), game.getCol());
+                int column = game.minimax(game.getGrid(),game.getCol(), d, true)[0];
+                if(column == -1){
+                    break;
                 }
-                isWon = game.insert(game.getGrid(), columnC, game.getCol());
+                isWon = game.insert(game.getGrid(), column, game.getCol(), 2);
                 game.turns++;
             }
             if(isWon) break;
             if(game.turns >= 49) break;
             game.turn = !game.turn;
         }
-        if(game.turns > 49 && !game.gameWon(1) && !game.gameWon(2)){
+        if(game.turns > 49 && !game.gameWon(game.getGrid(), 1) && !game.gameWon(game.getGrid(), 2)){
             System.out.println("There was no winner, game board is full");
         }else if(game.turn == true){
             System.out.println("Computer Loses, Player has Won");
         }else{
             System.out.println("Player Lost, Computer Won");
         }
-        game.display();
+        game.display(game.getGrid());
         sc.close();
     }
 }
